5e0fc3fdd62ca8905c2df4d466d6477ec3f146e5
AndreyNikolaev <you@example.com>
2013-11-08 15:22:55 +0200

    Before gesture recognizer

--- Clock/ColorPicker.m
+++ Clock/ColorPicker.m
@@ -57,6 +57,7 @@
         _oldColor = [self getRGBAsFromImageAtPoint:&p];
     }
     [self sendActionsForControlEvents:UIControlEventValueChanged];
+    NSLog(@"End Traking coords: X%f, Y%f", [touch locationInView:self].x , [touch locationInView:self].y);
 }
 
 -(BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event
@@ -66,28 +67,30 @@
         _oldColor = [self getRGBAsFromImageAtPoint:&p];
     }
     [self sendActionsForControlEvents:UIControlEventValueChanged];
+    NSLog(@"Continue  Traking coords: X%f, Y%f", [touch locationInView:self].x , [touch locationInView:self].y);
     return (YES);
 }
 
+
 - (void)cancelTrackingWithEvent:(UIEvent *)event
 {
+   
 }
 
 - (UIColor*)getRGBAsFromImageAtPoint: (CGPoint*)point
 {
-        unsigned char pixel[4] = {0};
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-        CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace,(CGBitmapInfo)kCGImageAlphaPremultipliedLast);
-        
-        CGContextTranslateCTM(context, -point->x, -point->y);
-        
-        [self.layer renderInContext:context];
-        
-        CGContextRelease(context);
-        CGColorSpaceRelease(colorSpace);
-        UIColor *color = [UIColor colorWithRed:pixel[0]/255.0 green:pixel[1]/255.0 blue:pixel[2]/255.0 alpha:pixel[3]/255.0];
-        
-        return color;
+    unsigned char pixel[4] = {0};
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace,(CGBitmapInfo)kCGImageAlphaPremultipliedLast);
+    
+    CGContextTranslateCTM(context, -point->x, -point->y);
+    
+    [self.layer renderInContext:context];
+    
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    UIColor *color = [UIColor colorWithRed:pixel[0]/255.0 green:pixel[1]/255.0 blue:pixel[2]/255.0 alpha:pixel[3]/255.0];
+    return color;
 }
 
 -(BOOL)isInView: (CGPoint*)point inView:(UIImageView*)view
