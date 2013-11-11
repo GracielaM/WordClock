c42f61ef7423b31c38aa91165bee43541d0b7ef8
AndreyNikolaev <you@example.com>
2013-11-11 15:19:31 +0200

    TapFlash

---
+++ Clock/TapFlash.m
@@ -0,0 +1,31 @@
+//
+//  TapFlash.m
+//  Clock
+//
+//  Created by Interns on 11/11/13.
+//  Copyright (c) 2013 graci. All rights reserved.
+//
+
+#import "TapFlash.h"
+
+@implementation TapFlash
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
